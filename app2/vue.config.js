37946d3eedf5f2f5a32127684afa18db98be3eca
wang meng <wangm@gih.cn>
2022-09-07 13:57:26 +0800

    提交qiankun分支

--- app2/vue.config.js
+++ app2/vue.config.js
@@ -94,12 +94,12 @@ module.exports = defineConfig({
                     lib_remote: `lib_remote@http://localhost:3003/remoteEntry.js`
                 },
                 // shared: ['vue']
-                shared: {
-                    vue: {
-                        eager: true,
-                        singleton: true,
-                    }
-                }
+                // shared: {
+                //     vue: {
+                //         eager: true,
+                //         singleton: true,
+                //     }
+                // }
             })
         ],
         optimization: {
